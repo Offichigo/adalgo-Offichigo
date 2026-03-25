8e26b997f3984b41682623034186a1158d93a049
Offichigo <oph.perianin@gmail.com>
2026-03-25 14:17:00 +0100

    first commit

---
+++ src/2-filterLetter.js
@@ -0,0 +1,40 @@
+/*
+This function checks whether a given argument is a letter or not.
+
+The function takes three parameters:
+
+- arg: the value to check
+- call: a callback function executed if the argument is a letter
+- call2: a callback function executed if the argument is NOT a letter
+
+If the argument is not a number, the function should execute the first callback.
+If the argument is a number, the function should execute the second callback.
+
+The function should return the result of the executed callback.
+
+For example:
+
+filterLetter("a", callback1, callback2)
+// should execute callback1
+
+filterLetter("8", callback1, callback2)
+// should execute callback2
+*/
+// Cette fonction vérifie si un argument donné est une lettre ou non.
+// La fonction prend trois paramètres :
+// - arg : la valeur à vérifier
+// - call : une fonction de rappel (callback) exécutée si l'argument est une lettre
+// - call2 : une fonction de rappel (callback) exécutée si l'argument N'est PAS une lettre
+// Si l'argument n'est pas un nombre, la fonction doit exécuter le premier callback.
+// Si l'argument est un nombre, la fonction doit exécuter le second callback.
+// La fonction doit retourner le résultat du callback exécuté.
+// Par exemple :
+// filterLetter("a", callback1, callback2)
+// // doit exécuter callback1
+// filterLetter("8", callback1, callback2)
+// // doit exécuter callback2
+export const filterLetter = (arg, call, call2) => {
+  const result = "";
+  const arg = "",
+  if typeof
+};
