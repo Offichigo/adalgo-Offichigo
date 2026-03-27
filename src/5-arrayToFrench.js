2caab56fd5fadabfa0636a50aec47fbabcc21750
Offichigo <oph.perianin@gmail.com>
2026-03-27 11:43:19 +0100

    ex 5 is done

--- src/5-arrayToFrench.js
+++ src/5-arrayToFrench.js
@@ -1,4 +1,7 @@
 /**
+ * Cette fonction prend un tableau de mots et le convertit en une phrase française.
+Les mots sont séparés par des virgules et les deux derniers mots sont séparés par "et".
+
  * This function takes an array of words and convert it into a french sentence. Words are separated by
  * commas and the two last words are separated by "et".
  *
@@ -9,4 +12,12 @@
  *      arrayToFrench(['un', 'deux', 'trois', 'soleil']); // returns "un, deux, trois et soleil"
  *      arrayToFrench(['noir', 'blanc']); // returns "noir et blanc"
  */
-export const arrayToFrench = (array) => {};
+export const arrayToFrench = (array) => {
+  if (array.length === 1) {
+    return array[0];
+  } else if (array.length > 1) {
+    const firstWords = array.slice(0, array.length - 1);
+    const lastWord = array[array.length - 1];
+    return firstWords.join(", ") + " et " + lastWord;
+  }
+};
--- src/5-arrayToFrench.test.js
+++ src/5-arrayToFrench.test.js
@@ -1,1 +1,16 @@
 // TODO: test arrayToFrench function with one, two and multiple words
+import { describe, it, expect } from "vitest";
+import { arrayToFrench } from "./5-arrayToFrench";
+
+describe("arrayToFrench", () => {
+  it("arrayToFrench retourne un tableau de mots français pour en faire une phrase française", () =>
+    expect(arrayToFrench(["un", "deux", "trois", "soleil"])).toBe(
+      "un, deux, trois et soleil",
+    ));
+
+  it("arrayToFrench retourne un tableau deux mots français pour en faire une phrase française séparé les mots avec un et ", () =>
+    expect(arrayToFrench([`noir`, `blanc`])).toBe("noir et blanc"));
+
+  it("arrayToFrench retourne un tableau d'un mot", () =>
+    expect(arrayToFrench(["noir"])).toBe("noir"));
+});
